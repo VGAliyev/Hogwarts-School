aa128188b64cc3f8efdb3de8ae6a18496a04e837
VGAliyev <v.aliev84@yandex.ru>
2024-02-04 21:54:06 +0400

    POST methode uploadAvatar transfer from AvatarController to StudentController

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
 
@@ -23,15 +22,6 @@ public class AvatarController {
 
     public AvatarController(AvatarService avatarService) {
         this.avatarService = avatarService;
-    }
-
-    @PostMapping(value = "{studentId}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
-        if (avatar.getSize() > 1024 * 300) {
-            return ResponseEntity.badRequest().body("File is too big!");
-        }
-        avatarService.uploadAvatar(studentId, avatar);
-        return ResponseEntity.ok().build();
     }
 
     @GetMapping(value = "{id}/avatar-from-db")
