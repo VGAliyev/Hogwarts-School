dcb02fc8c771caf3c683be019c70e29fc5de0481
VGAliyev <v.aliev84@yandex.ru>
2024-02-06 17:33:37 +0400

    StudentControllerTest.java complete

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--INSERT INTO avatar (id, data, file_path, file_size, media_type, student_id) VALUES (1, '', 'file-path', 1024, 'media-type', 1);
+
+INSERT INTO faculty (id, color, name) VALUES (1, 'Red', 'Gryffindor');
+INSERT INTO faculty (id, color, name) VALUES (2, 'Green', 'Slytherin');
+
+INSERT INTO student (id, age, name, faculty_id) VALUES (1, 15, 'Harry Potter', 1);
+INSERT INTO student (id, age, name, faculty_id) VALUES (2, 16, 'Hermione Granger', 1);
+INSERT INTO student (id, age, name, faculty_id) VALUES (3, 17, 'Draco Malfoy', 2);
