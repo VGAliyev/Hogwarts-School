4b078204834aeed9d52f36f1e6adcf447181fbfb
VGAliyev <v.aliev84@yandex.ru>
2024-02-07 22:58:25 +0400

    StudentControllerWebMvcTest complete

--- src/test/java/ru/hogwarts/school/constants/TestConstants.java
+++ src/test/java/ru/hogwarts/school/constants/TestConstants.java
@@ -8,4 +8,7 @@ public class TestConstants {
     public final static Faculty SLYTHERIN = new Faculty(2L, "Slytherin", "Green");
     public final static Student HARRY = new Student(1L, "Harry Potter", 11, GRYFFINDOR);
     public final static Student HERMIONE = new Student(2L, "Hermione Granger", 12, GRYFFINDOR);
+
+    // For WebMvcTest
+    public final static Student JOHN = new Student(1L, "John", 20);
 }
